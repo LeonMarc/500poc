527a491627ebd5884f0a30f8adbe4baf548240e4
LeonMarc <marcillaleon@gmail.com>
2025-03-05 21:18:57 -0300

    main

--- cypress.config.js
+++ cypress.config.js
@@ -9,7 +9,7 @@ module.exports = defineConfig({
         // Enable upload to Argos only when it runs on CI.
         uploadToArgos: !!process.env.CI,
         // Set your Argos token (required only if you don't use GitHub Actions).
-        token: "ARGOS_TOKEN=argos_379d0c94361b7b3243e2ff32dda7693caa",
+        token: "argos_379d0c94361b7b3243e2ff32dda7693caa",
       });
 
       // include any other plugin code...
--- cypress/e2e/homepage.cy.js
+++ cypress/e2e/homepage.cy.js
@@ -1,4 +1,5 @@
 it("screenshot homepage", async ({ page }) => {
     cy.visit("https://500designs.com/");
+    cy.wait(4000)
     cy.argosScreenshot("homepage");
   });
